af0255e86c239941def3af698767b9bcc17ff687
Ana Laura <rg.analaura9@gmail.com>
2019-03-31 01:51:05 -0600

    Ahora si

--- Ashop.c
+++ Ashop.c
@@ -1,36 +1,14 @@
-Ashop.c#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-
-typedef struct {
-    char nombre[15];
-    char apellido[15];
-    char tipoEmpleado[15];
-    char usuario[15];
-    char contrasenio[15];
-    char area[15];
-    int sueldo;
-}Empleado;
+#include <string.h>
+#include "Login.h"
 
 int main() {
 	int i,op;
-	do {
-		printf("***MENU***\n");
-		printf("1. Registrar empleado.\n");
-		printf("2. Iniciar sesion\n");
-		printf("3. Salir\n");
-		printf("R: ");
-		printf("Prueba...")
-		scanf("%d",&op);
-		switch (op) {
-			case 1:
-				printf("REGISTRAR EMPLEADO");
-			break;
-			case 2:
-				
-			break;
-		}
-	} while(op!=3);
-	
+	int tipo;
+	printf("*** BIENVENIDO A A-SHOP ***\n");
+	printf("Iniciar sesion\n");
+	tipo=correrLogin();
 	
 	return 0;
 }
