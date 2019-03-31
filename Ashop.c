94ff9d2009274b5be17ccc29c85c496b76ae0942
Rolan <miguelrolando123@gmail.com>
2019-03-30 22:03:57 -0600

    Falta un día...

---
+++ Ashop.c
@@ -0,0 +1,35 @@
+Ashop.c#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char nombre[15];
+    char apellido[15];
+    char tipoEmpleado[15];
+    char usuario[15];
+    char contrasenio[15];
+    char area[15];
+    int sueldo;
+}Empleado;
+
+int main() {
+	int i,op;
+	do {
+		printf("***MENU***\n");
+		printf("1. Registrar empleado.\n");
+		printf("2. Iniciar sesion\n");
+		printf("3. Salir\n");
+		printf("R: ");
+		scanf("%d",&op);
+		switch (op) {
+			case 1:
+				printf("REGISTRAR EMPLEADO");
+			break;
+			case 2:
+				
+			break;
+		}
+	} while(op!=3);
+	
+	
+	return 0;
+}
