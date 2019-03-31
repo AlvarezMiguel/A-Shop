309454a384fe21cc279367031720f77048d778a2
Ana Laura <rg.analaura9@gmail.com>
2019-03-31 09:01:12 -0600

    Modificando

--- Empleados.h
+++ Empleados.h
@@ -29,6 +29,9 @@ void admin() {
 		printf("ADMINISTRADORES\n");
 		printf("1. Dar de baja departamento\n");
 		printf("2. Dar de alta departamento \n");
+		printf("3. Modificar informacion de empleados\n");
+		printf("4. Agregar empleado\n");
+		printf("5. Eliminar empleado\n");
 		scanf("%d",&op);
 		if(op==1) {
 			bajaDep();
